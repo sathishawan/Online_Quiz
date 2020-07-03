cd4b0801aef89ed9b6fd575dbd20920b916138b0
Keerthana C <keerthana@aims.awaninfotech.com>
2020-07-03 16:31:27 +0530

    keerthi modified file

--- src/main/java/com/Registration/Registration/controllers/AuthController.java
+++ src/main/java/com/Registration/Registration/controllers/AuthController.java
@@ -63,12 +63,13 @@ public class AuthController {
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
 
         Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken
+                new UsernamePasswordAuthenticationToken(
 
                         loginRequest.getEmail(),
                         loginRequest.getPassword()
                 )
         );
+
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String jwt = jwtUtils.generateJwtToken(authentication);
